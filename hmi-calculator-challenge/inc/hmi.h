769fb516bf2c9fb9fc20e5d04efe3cc78ad65d5c
Caio Ruppert <ruppertcaio@gmail.com>
2026-09-14 18:59:08 -0300

    integra math_ops via tabela de despacho

--- hmi-calculator-challenge/inc/hmi.h
+++ hmi-calculator-challenge/inc/hmi.h
@@ -1,10 +1,15 @@
 #ifndef HMI_H
 #define HMI_H
 
+typedef enum {
+    HMI_CONTINUE,
+    HMI_EXIT
+} HmiStatus;
+
 // Inicializador
 void hmi_init(void);
 
 // Rotina principal
-void hmi_run(void);
+HmiStatus hmi_run(void);
 
 #endif // HMI_H
